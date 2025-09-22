b1aa713f35752886c43c7c408924f84fbea2b057
Obodi <saulius@obodi.eu>
2025-09-22 20:26:08 +0300

    index

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const schedule = require('node-schedule');
+const http = require('http');
+
+
+// Create a server object
+const server = http.createServer((req, res) => {
+  // Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Send the response body as 'Hello, World!'
+  res.end('Hello, World!\n');
+});
+
+
+const PORT = 3000;
+// Define the port to listen on const PORT = 3000;
+
+// Start the server and listen on the specified port
+server.listen(PORT, 'localhost', () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
 
 const job = schedule.scheduleJob('* * * * *', function () {
     console.log('The answer to life, the universe, and everything!');
 });
+
